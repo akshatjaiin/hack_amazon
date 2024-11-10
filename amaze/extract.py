1dd3f87ccf784f5d5e8ee12c63ec4ef378680f23
Akshat Jain <akshat0@yahoo.com>
2024-11-11 00:08:49 +0530

    fixed

--- amaze/extract.py
+++ amaze/extract.py
@@ -1,3 +1,15 @@
+# import
+import os
+import json
+from dotenv import load_dotenv
+import requests
+import instaloader
+from bs4 import BeautifulSoup
+import cv2
+from moviepy.editor import VideoFileClip
+import time
+import base64
+
 def download_image(url, folder_path, image_name):
     """Downloads an image from a URL and saves it to a specified folder."""
     try:
@@ -19,13 +31,64 @@ def download_image(url, folder_path, image_name):
         print(f"Error downloading image {image_name}: {e}")
 
 
+def process_media(folder_path, seconds_per_frame=2):
+    print("process media called")
+    base64Frames = []   
+    # Iterate over each file in the download folder
+    for file_name in os.listdir(folder_path):
+        print(f"file name: {file_name}")
+        file_path = os.path.join(folder_path, file_name)
+        # handling image
+        if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            with open(file_path, "rb") as image_file:
+                base64Frames.append(base64.b64encode(image_file.read()).decode("utf-8"))
+
+        elif file_name.lower().endswith(('.mp4', '.avi', '.mov', '.mkv')):
+        
+            base_file_path, _ = os.path.splitext(file_path)
+
+            video = cv2.VideoCapture(file_path)
+            total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+            fps = video.get(cv2.CAP_PROP_FPS)
+            frames_to_skip = int(fps * seconds_per_frame)
+            curr_frame=0
+
+            # Loop through the video and extract frames at specified sampling rate
+            while curr_frame < total_frames - 1:
+                video.set(cv2.CAP_PROP_POS_FRAMES, curr_frame)
+                success, frame = video.read()
+                if not success:
+                    break
+                _, buffer = cv2.imencode(".jpg", frame)
+                base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
+                curr_frame += frames_to_skip
+            video.release()
+
+            # Extract audio from video
+            audio_path = f"{base_file_path}.mp3"
+            clip = VideoFileClip(file_path)
+            clip.audio.write_audiofile(audio_path, bitrate="32k")
+            clip.audio.close()
+            clip.close()
+
+            print(f"Extracted {len(base64Frames)} frames")
+            print(f"Extracted audio to {audio_path}")
+
+    audio_path = audio_path if audio_path else None
+    media = {
+        'frames': base64Frames,
+        'audio_path': audio_path
+    }
+    return media
+
+
 def extract_instagram_post_info(url):
     """Extracts information from an Instagram post and downloads images using instaloader."""
     loader = instaloader.Instaloader(
         download_comments=False,
         download_geotags=True,
         download_pictures=True,
-        download_video_thumbnails=False,
+        download_video_thumbnails=True,
         save_metadata=True
     )
     shortcode = url.split('/')[-2]
@@ -39,8 +102,8 @@ def extract_instagram_post_info(url):
             'images': [node.display_url for node in post.get_sidecar_nodes()] if post.is_video else [post.url]  # Handle video posts
         }
         # Download the post to a folder named after the shortcode
-
-        loader.download_post(post, target=shortcode)
+        folder_name = "download"
+        loader.download_post(post, target=folder_name)
         return post_info
     except Exception as e:
         print(f"Error extracting Instagram post: {e}")
--- amaze/views.py
+++ amaze/views.py
@@ -1,80 +1,36 @@
 # django modules
-from django.urls import reverse
 from django.db import IntegrityError
-from django.contrib.sessions.models import Session
+from django.http import HttpResponse
 from django.shortcuts import  render, get_object_or_404
-from django.http import HttpResponse, JsonResponse  
 
-# import
 import os
-import json
-from dotenv import load_dotenv
-import requests
-import instaloader
-from . import constant 
-from . import extract
-from openai import OpenAI
-from .amazon_product_search import amazon_product_search 
 from json import loads
-import cv2
-from moviepy.editor import VideoFileClip
-import time
-import base64
+from openai import OpenAI
+from dotenv import load_dotenv
+from . import constant, extract
+from .amazon_product_search import amazon_product_search
+import json
 load_dotenv()
 client = OpenAI(
     # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-
-def process_video(video_path, seconds_per_frame=2):
-    base64Frames = []   
-    base_video_path, _ = os.path.splitext(video_path)
-
-    video = cv2.VideoCapture(video_path)
-    total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps = video.get(cv2.CAP_PROP_FPS)
-    frames_to_skip = int(fps * seconds_per_frame)
-    curr_frame=0
-
-    # Loop through the video and extract frames at specified sampling rate
-    while curr_frame < total_frames - 1:
-        video.set(cv2.CAP_PROP_POS_FRAMES, curr_frame)
-        success, frame = video.read()
-        if not success:
-            break
-        _, buffer = cv2.imencode(".jpg", frame)
-        base64Frames.append(base64.b64encode(buffer).decode("utf-8"))
-        curr_frame += frames_to_skip
-    video.release()
-
-    # Extract audio from video
-    audio_path = f"{base_video_path}.mp3"
-    clip = VideoFileClip(video_path)
-    clip.audio.write_audiofile(audio_path, bitrate="32k")
-    clip.audio.close()
-    clip.close()
-
-    print(f"Extracted {len(base64Frames)} frames")
-    print(f"Extracted audio to {audio_path}")
-    return base64Frames, audio_path
-
-
-
-def ask_ai(base64Frames,transcription):
+def ask_ai(frames,transcription,content):
     try:
         prompt = constant.prompt
-        print("post text inside function: "+transcription.text)
+        content = json.dumps(content)
+        print("post text inside function: "+content)
 
             ## Generate a summary with visual and audio
         response = client.chat.completions.create(
             model="gpt-4o-mini",
             messages=[
-            {"role": "system", "content":constant.prompt},
+            {"role": "system", "content":constant.prompt+"i m feeding you few post related content " + content},
             {"role": "user", "content": [
-                "These are the frames from the video.",
+                "These are the frames from the post.",
                 *map(lambda x: {"type": "image_url", 
-                                "image_url": {"url": f'data:image/jpg;base64,{x}', "detail": "low"}}, base64Frames),
+                                "image_url": {"url": f'data:image/jpg;base64,{x}', "detail": "low"}}, frames),
                 {"type": "text", "text": f"The audio transcription is: {transcription.text}"}
                 ],
             }
@@ -102,27 +58,26 @@ def index(request):
             'author': "Unknown"
         }
 
-        if post_info['images']:
               # Replace None values with default values for any missing or None fields
-            for key, default_value in default_values.items():
-                post_info[key] = post_info.get(key, default_value) or default_value
-            response = ask_ai(post_info['images'],post_info['content'])
+        
+        response = ["empty list"]
+        
+        # response = ask_ai(base64Frames,transcription)
+        media = extract.process_media("download")
+
+        if media['audio_path'] != None:
+            # Transcribe the audio
+            transcription = client.audio.transcriptions.create(
+                model="whisper-1",
+                file=open(media['audio_path'], "rb"),
+            )
         else:
-            print("missing video")
+            transcription = {'text': 'post has no transcription'}
 
-
-        # # Create a folder to save images
-        # folder_name = "down_image"
-        # save_images(post_info['images'], folder_name)
-
-           
-        if(response == None):
-            return HttpResponse("<p>Error, while asking gpt about product</p>")
-        print(response.choices[0].message.content.split("```"))
-        ai_response= response.choices[0].message.content.replace("```","").replace("json","").replace("\n","")
-        ai_response = loads(ai_response)
-        print(ai_response)
-        amazon_search_result= amazon_product_search(f'{ai_response.get("query")},' + ai_response.get("brand"))
+        response=ask_ai(frames=media['frames'], transcription=transcription, content=post_info)
+        query = response.choices[0].message.content
+        print(query)
+        amazon_search_result= amazon_product_search(query)
         print(amazon_search_result)
 
         return render(request, "amaze/index.html", {
